87d928682a8e05977623c392d84ace7aff147913
Asjad Athick <asjadathick@gmail.com>
2022-02-18 15:39:05 +1100

    WIP

--- monitoring-agent-instance/main.tf
+++ monitoring-agent-instance/main.tf
@@ -15,6 +15,13 @@ resource "aws_security_group" "cloud_security_monitor-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
